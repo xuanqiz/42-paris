dc655bb31d5d67929d68a88a8fe08ff277735b17
Xuanqi ZHAO <xzhao@e1r13p10.42.fr>
2019-10-22 21:36:17 +0200

    check

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: xzhao <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/12 18:05:26 by xzhao             #+#    #+#             */
-/*   Updated: 2019/10/22 21:11:12 by xzhao            ###   ########.fr       */
+/*   Updated: 2019/10/22 21:34:49 by xzhao            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,18 @@
 int	ft_atoi(const char *str)
 {
 	int	nb;
-	int	neg;
+	int	sign;
 	int	i;
 
 	nb = 0;
-	neg = 0;
+	sign = 0;
 	i = 0;
 	while ((str[i] <= 13 && str[i] >= 9) || str[i] == 32)
 		i++;
 	if (str[i] == '+' || str[i] == '-')
 	{
 		if (str[i] == '-')
-			neg = 1;
+			sign = 1;
 		i++;
 	}
 	while (str[i] <= '9' && str[i] >= '0')
@@ -34,7 +34,7 @@ int	ft_atoi(const char *str)
 		nb = (nb * 10) - 48 + str[i];
 		i++;
 	}
-	if (neg == 1)
+	if (sign == 1)
 		return (-nb);
 	else
 		return (nb);
