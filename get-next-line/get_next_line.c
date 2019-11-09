e42dd1a4ea3f50a4f22a856c1b6782b98ebba871
Xuanqi ZHAO <xzhao@e2r2p22.42.fr>
2019-11-09 17:44:41 +0100

    start

--- get-next-line/get_next_line.c
+++ get-next-line/get_next_line.c
@@ -6,8 +6,25 @@
 /*   By: xzhao <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/09 17:33:39 by xzhao             #+#    #+#             */
-/*   Updated: 2019/11/09 17:33:41 by xzhao            ###   ########.fr       */
+/*   Updated: 2019/11/09 17:44:23 by xzhao            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
+int get_next_line(int fd, char **line)
+{
+	char buf[BUFFER_SIZE - BUFFER_SIZE + 2];
+	int	ret;
+	
+	if ( fd < -1 || !line || 
+		!(line = malloc(sizeof(char) + 1)))
+		return (-1);
+	
+	return (0);
+
+
+}
+
+/*
+** 
